8ab311905db94444211ea2ff440584be74aeed2f
Khurshid Usmanov <khurriccan@login.snowmass21.io>
2023-06-09 15:35:24 -0500

    Template commit

---
+++ TemplatePackage/TemplatePackage/khurshid_works.hxx
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <TH1F.h>
+
+#include <marlin/Processor.h>
+
+//! A template processor that should be modified to do what you want.
+/**
+ * Implements a loop over the MCParticle collection and creates an output
+ * colleciton passing certain criteria. This provides rovides examples of:
+ *  - using parameters to configure a processor
+ *  - opening collections
+ *  - outputing histograms
+ */
+class khurshid_works  : public marlin::Processor
+{
+public:
+  virtual Processor*  newProcessor() { return new khurshid_works ; }
+
+  khurshid_works(const khurshid_works&) = delete ;
+  khurshid_works& operator =(const khurshid_works&) = delete ;
+  khurshid_works() ;
+
+  /** Called at the begin of the job before anything is read.
+   * Use to initialize the processor, e.g. book histograms.
+   */
+  virtual void init() ;
+
+  /** Called for every run.
+   */
+  virtual void processRunHeader( LCRunHeader* run ) ;
+
+  /** Called for every event - the working horse.
+   */
+  virtual void processEvent( LCEvent * evt ) ; 
+
+  virtual void check( LCEvent * evt ) ; 
+
+  /** Called after data processing for clean up.
+   */
+  virtual void end() ;  
+
+private:
+  //! Input Collection
+//  std::vector<std::string> _inputEcalCollections{};
+   std::string _inputEcalCollection{};
+  //! Output Collection
+ 
+  std::vector<std::string> _outputEcalCollections{};
+  
+  //! Minimum pT for particle filter
+  float _minPt = 1;
+
+  //! Output histogram
+  TH1 *_h_pt = nullptr;
+};
---
+++ TemplatePackage/src/khurshid_works.cxx
@@ -0,0 +1,88 @@
+#include "TemplatePackage/khurshid_works.hxx"
+
+#include <EVENT/LCCollection.h>
+#include <EVENT/MCParticle.h>
+
+#include <marlin/AIDAProcessor.h>
+#include <marlin/Global.h>
+#include <cmath>
+#include <IMPL/CalorimeterHitImpl.h>
+#include <EVENT/LCCollection.h>
+#include <IMPL/LCCollectionVec.h>
+
+khurshid_works akhurshid_works ;
+
+khurshid_works::khurshid_works()
+  : Processor("khurshid_works") {
+  // modify processor description
+  _description = "Khurshid's Processor stores a collection of ECalorimeter hits and outputs a copy." ;
+   
+
+
+
+  // register steering parameters: name, description, class-variable, default value
+ /* registerProcessorParameter("MinPt",
+                             "Minimum particle pT.",
+                              _minPt,
+                             0.5);*/
+
+  std::vector<std::string> ecalCollections; // 
+  ecalCollections.push_back(std::string("EcalBarrelCollection"));
+  
+  registerInputCollection( LCIO::CALORIMETERHIT,
+                            "ECALOutputCollection0" ,
+                            "ECAL Collection of real Hits" ,
+                            //_inputEcalCollections.at(0),
+                         _inputEcalCollection,   std::string("ECALBarrel") );
+  std::cout << "crush point_1" << std::endl;
+  _outputEcalCollections.push_back(std::string("ECALBarrel"));      
+
+
+  registerOutputCollection( LCIO::CALORIMETERHIT, // output - copy of ecal hits 
+                            "ECALOutputCollection0" ,
+                            "Khurshid's copy of ECAL Collection of real Hits" ,
+                            _outputEcalCollections.at(0),
+                            std::string("ECALBarrel") );   
+std::cout << "crush point" << _outputEcalCollections.at(0)<< std::endl;
+
+}
+
+
+void khurshid_works::init() {
+  // Print the initial parameters
+ // printParameters() ;
+
+  // Create useful histograms
+ /* marlin::AIDAProcessor::histogramFactory(this);
+  _h_pt = new TH1F("pt", "", 100, 0., 10);*/
+}
+
+void khurshid_works::processRunHeader( LCRunHeader* /*run*/) {
+}
+
+void khurshid_works::processEvent( LCEvent * evt ) {
+  //
+  // Get object required collections and create lists
+  // to keep track of unsaved objects.
+
+  // Loop over MCParticles
+ // LCCollection* inputCol = evt->getCollection(_inputEcalCollections.at(0));
+    LCCollection* inputCol = evt->getCollection(_inputEcalCollection);
+  if( inputCol->getTypeName() != LCIO::CALORIMETERHIT ) {
+    throw EVENT::Exception( "Invalid collection type: " + inputCol->getTypeName() ) ;
+  }
+
+  std::cout << "Found " << inputCol->getNumberOfElements() << " ECalorimeter hits." << std::endl;
+
+  for(uint32_t i=0;i<inputCol->getNumberOfElements();i++) {
+	 std::vector<const CalorimeterHitImpl*> myVector;
+	 const CalorimeterHitImpl *hit_copy=static_cast<const CalorimeterHitImpl*>(inputCol->getElementAt(i));
+         myVector.push_back(hit_copy);
+         std::cout << "Found " << hit_copy<< " ECalorimeter hits." << std::endl;
+   }
+}
+
+void khurshid_works::check( LCEvent * /*evt*/ )
+{ }
+void khurshid_works::end()
+{ }
