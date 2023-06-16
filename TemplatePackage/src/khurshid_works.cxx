621aef4d089ae8c2c266ccdc5983cf4d4ac8fd6d
Khurshid Usmanov <khurriccan@login.snowmass21.io>
2023-06-15 19:28:54 -0500

    Commit June 15th: updated processor for a complete set of ECAL collections

--- TemplatePackage/src/khurshid_works.cxx
+++ TemplatePackage/src/khurshid_works.cxx
@@ -18,8 +18,6 @@ khurshid_works::khurshid_works()
   _description = "Khurshid's Processor stores a collection of ECalorimeter hits and outputs a copy." ;
    
 
-
-
   // register steering parameters: name, description, class-variable, default value
  /* registerProcessorParameter("MinPt",
                              "Minimum particle pT.",
@@ -38,29 +36,9 @@ khurshid_works::khurshid_works()
                             "ECAL Collection of real Hits" ,
                             _inputEcalCollections,    
                             ecalCollections) ;
-                                       
-/*registerInputCollection( LCIO::CALORIMETERHIT,
-                            "ECALOutputCollection_kh_input" ,
-                            "ECAL Collection of real Hits" ,
-                            //_inputEcalCollections.at(0),
-                            _inputEcalCollectionion,  
-                             std::string("ECALBarrel") );
-
-  registerInputCollection( LCIO::CALORIMETERHIT,
-                            "ECALOutputCollection1" ,
-                            "ECAL Collection of real Hits" ,
-                            _inputEcalCollection.at(1),
-                            std::string("ECALEndcap") );
-
- registerInputCollection( LCIO::CALORIMETERHIT,
-                            "ECALOutputCollection2" ,
-                            "ECAL Collection of real Hits" ,
-                            _inputEcalCollection.at(2),
-                            std::string("ECALOther") ) ;*/
-
 
    
-  std::cout << "crush point_1" << std::endl;
+// std::cout << "crush point_1" << std::endl;
 
   _outputEcalCollections.push_back(std::string("ECALBarrel"));
   _outputEcalCollections.push_back(std::string("ECALEndcap"));
@@ -86,7 +64,7 @@ khurshid_works::khurshid_works()
                             std::string("ECALOther_output") ) ;
    
 
-  std::cout << "crush point_2" << _outputEcalCollections.at(0)<< std::endl;
+// std::cout << "crush point_2" << _outputEcalCollections.at(0)<< std::endl;
 
 }
 
@@ -104,51 +82,58 @@ void khurshid_works::processRunHeader( LCRunHeader* /*run*/) {
 }
 
 void khurshid_works::processEvent( LCEvent * evt ) {
-
-   LCCollection* inputCol = evt->getCollection(_inputEcalCollections.at(0));
-   //LCCollection* inputCol_Endcap = evt->getCollection(_inputEcalCollections.at(1));
- //  LCCollection* inputCol_Other = evt->getCollection(_inputEcalCollections.at(2));
-   //LCCollection* inputCol = evt->getCollection(_inputEcalCollection);
  
-   if( inputCol->getTypeName() != LCIO::CALORIMETERHIT ) {
+ /*  if( inputCol->getTypeName() != LCIO::CALORIMETERHIT ) {
     throw EVENT::Exception( "Invalid collection type: " + inputCol->getTypeName() ) ;
-  }
-
-  /*if( inputCol_Other->getTypeName() != LCIO::CALORIMETERHIT ) {
-    throw EVENT::Exception( "Invalid collection type: " + inputCol_Other->getTypeName() ) ;
   }*/
-
-
-
-  std::cout << "Found " << inputCol->getNumberOfElements() << " ECalorimeter hits." << std::endl;
-  LCCollectionVec *ecalcol_kh = new LCCollectionVec(LCIO::CALORIMETERHIT);
-  _flag.setBit(LCIO::CHBIT_LONG);
-  _flag.setBit(LCIO::RCHBIT_TIME);
-  ecalcol_kh->setFlag(_flag.getFlag());
   
-  for(uint32_t i=0;i<inputCol->getNumberOfElements();i++) {
-	// CalorimeterHitImpl *hit_copy=static_cast<CalorimeterHitImpl*>(inputCol->getElementAt(i));
-         CalorimeterHitImpl *hit_copy=new CalorimeterHitImpl(*static_cast<CalorimeterHitImpl*>(inputCol->getElementAt(i)));
-         if ( hit_copy->getEnergy() > 0.5 ) {
-            ecalcol_kh->addElement(hit_copy); 
- 	    std::cout << "Found " << hit_copy<< " ECalorimeter hits above 0.5 GeV." << std::endl;
-         }
-          
-   }
-  
+   for (int i = 0; i < _inputEcalCollections.size(); i++) {
+    std::string colName =  _inputEcalCollections[i] ;
 
- // std::string colName =  _inputEcalCollection;
- // LCCollection * col = evt->getCollection( colName.c_str() ) ;
-  std::string initString = inputCol->getParameters().getStringVal(LCIO::CellIDEncoding);
-  std::cout << "crush point 3 " << std::endl;
-  
-  ecalcol_kh->parameters().setValue(LCIO::CellIDEncoding,initString);
-  evt->addCollection(ecalcol_kh,_outputEcalCollections[0].c_str());
-  std::cout << "crush point 4  " << std::endl;
+    streamlog_out ( DEBUG ) << "looking for collection: " << colName << std::endl;
+	 try{ 
+  		LCCollection* inputCol = evt->getCollection(colName);
+    
 
+
+   		 if ( colName.find("dummy")!=std::string::npos ) {
+      			streamlog_out ( DEBUG ) << "ignoring input ECAL collection name (looks like dummy name)" << colName <<std::endl;
+     			 continue;
+   			 }
+    
+   		 LCCollectionVec *ecalcol_kh = new LCCollectionVec(LCIO::CALORIMETERHIT);
+      		 ecalcol_kh->setFlag(_flag.getFlag());
+   
+ 		 for(uint32_t i=0;i<inputCol->getNumberOfElements();i++) {
+		// CalorimeterHitImpl *hit_copy=static_cast<CalorimeterHitImpl*>(inputCol->getElementAt(i));
+        	 CalorimeterHitImpl *hit_copy=new CalorimeterHitImpl(*static_cast<CalorimeterHitImpl*>(inputCol->getElementAt(i)));
+       		 
+                	 if ( hit_copy->getEnergy() > 0.5 ) {
+            			ecalcol_kh->addElement(hit_copy); 
+ 	   		 	std::cout << "Found " << hit_copy<< " ECalorimeter hits above 0.5 GeV." << std::endl;
+         			}
+    
+   			}
+
+  		std::string initString = inputCol->getParameters().getStringVal(LCIO::CellIDEncoding);
+  //		std::cout << "crush point 3 " << std::endl;
+
+  		ecalcol_kh->parameters().setValue(LCIO::CellIDEncoding,initString);
+  		evt->addCollection(ecalcol_kh,_outputEcalCollections[i].c_str());
+  //		std::cout << "crush point 4  " << std::endl;
+
+   		}
+
+  	 catch (DataNotAvailableException &e){
+      		streamlog_out(DEBUG) << "could not find input ECAL collection " << colName << std::endl;
+      		}   
+
+   }	
+  
 
 
 }
+
 
 void khurshid_works::check( LCEvent * /*evt*/ )
 { }
