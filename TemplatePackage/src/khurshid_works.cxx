51da78a68f646923187867d92f5b930d1cb2a9ef
Khurshid Usmanov <khurriccan@login.snowmass21.io>
2023-06-22 23:55:20 -0500

    extra changes

--- TemplatePackage/src/khurshid_works.cxx
+++ TemplatePackage/src/khurshid_works.cxx
@@ -31,7 +31,7 @@ khurshid_works::khurshid_works()
   ecalCollections.push_back(std::string("ECALEndcap"));
   ecalCollections.push_back(std::string("ECALOther"));
   
- registerInputCollections( LCIO::CALORIMETERHIT,
+  registerInputCollections( LCIO::CALORIMETERHIT,
                             "ECALOutputCollection_kh_input" ,
                             "ECAL Collection of real Hits" ,
                             _inputEcalCollections,    
@@ -51,7 +51,7 @@ khurshid_works::khurshid_works()
                             _outputEcalCollections.at(0),
                             std::string("ECALBarrel_output") );
    
- registerOutputCollection( LCIO::CALORIMETERHIT,
+  registerOutputCollection( LCIO::CALORIMETERHIT,
                             "ECALEndcaps_kh" ,
                            "Khurshid's copy of ECAL Endcaps Collection of real Hits " ,
                             _outputEcalCollections.at(1),
@@ -63,10 +63,143 @@ khurshid_works::khurshid_works()
                             _outputEcalCollections.at(2),
                             std::string("ECALOther_output") ) ;
    
+ //HCAL Collection
+    
+  std::vector<std::string> hcalCollections;
+  hcalCollections.push_back(std::string("HCALBarrel"));
+  hcalCollections.push_back(std::string("HCALEndcap "));
+  hcalCollections.push_back(std::string("HCALOther"));
 
+  registerInputCollections( LCIO::CALORIMETERHIT,
+			    "HCALOutputCollection_kh_input",
+                            "HCAL Collection of real Hits", 
+                             _inputHcalCollections,	
+		             hcalCollections);
+
+  _outputHcalCollections.push_back(std::string("HCALBarrel"));
+  _outputHcalCollections.push_back(std::string("HCALEndcap"));
+  _outputHcalCollections.push_back(std::string("HCALOther"));
+ 
+  registerOutputCollection( LCIO::CALORIMETERHIT,
+                            "HCALBarrel_kh", 
+                            "Khurshid's copy of HCAL Barrel Collection of real Hits",
+                            _outputHcalCollections.at(0),
+                            std::string("HCALBarrel_output") );
+  
+  registerOutputCollection( LCIO::CALORIMETERHIT,
+                            "HCALEndcap_kh",
+                            "Khurshid's copy of HCAL Endcaps Collection of real Hits",
+                            _outputHcalCollections.at(1),
+                            std::string("HCALOther_output") ) ;
+  
+  registerOutputCollection( LCIO::CALORIMETERHIT,
+                            "HCALOther_kh",
+                            "Khurshid's copy of HCAL Others Collection of real Hits",
+                            _outputHcalCollections.at(2),
+                            std::string("HCALOther_output") );
 // std::cout << "crush point_2" << _outputEcalCollections.at(0)<< std::endl;
 
 }
+
+bool applyDifferentialThresholdEcalBIB(CalorimeterHitImpl * calhit, bool useCLIC, bool useCrilin) {
+  bool pass = false;
+
+  if (useCLIC==false && useCrilin==false) pass = true;
+
+  if (useCLIC==true){
+	  
+    float x = calhit->getPosition()[0];
+    float y = calhit->getPosition()[1];
+    float z = calhit->getPosition()[2];
+    float d = sqrt(x*x+y*y+z*z);
+    float R = sqrt(x*x+y*y);
+    float theta = acos(z/d);
+
+    float theta_bins[3+1] = {1.57-1.02,1.57-0.27,1.57+0.27,1.57+1.02};
+    float R_bins[5+1] = {1500,1570,1600,1650,1710,1770};
+
+    float th_barrel[5][3] = {18.2731,  17.1456,  18.4469,
+                    24.7357,  21.9555,  24.6477,
+                    33.8206,  30.3771,  33.8971,
+                    43.646,   42.9506,  43.3244,
+                    45.6661,  47.0891,  46.0237};
+
+    float std_barrel[5][3] = {32.9455,  31.5414,  34.2989,
+                        55.6836,  48.5732,  55.6375,
+                        77.1442,  71.8559,  76.0541,
+                        93.3592,  94.8317,  91.6914,
+                        96.7721,  96.9367,  94.6892};
+
+    for (int i_theta=0; i_theta<3; i_theta++){
+      for (int i_R=0; i_R<5; i_R++){
+        if (theta>theta_bins[i_theta] && theta<theta_bins[i_theta+1] && R>R_bins[i_R] && R<R_bins[i_R+1]) {
+          if (calhit->getEnergy()>(th_barrel[i_R][i_theta]+2*std_barrel[i_R][i_theta])*0.001) {pass = true;
+                                                                  calhit->setEnergy(calhit->getEnergy()-th_barrel[i_theta][i_R]*0.001);
+                                                                 }
+        }
+      }
+    } 
+
+
+    }
+
+    if (useCrilin==true){
+
+    float x = calhit->getPosition()[0];
+    float y = calhit->getPosition()[1];
+    float z = calhit->getPosition()[2];
+
+     //Rotation of the dodecaedra
+     
+         float phi = acos(x/sqrt(x*x+y*y));
+    
+         if (y<0) phi = -phi;
+    
+         float xphi = phi + 3.14159/12.;
+    
+         if (xphi<0) xphi = 2*3.14159+xphi;
+     
+         int nphi = xphi*6./3.14159;
+   
+         float delta_phi = 3.14159/2.-2*3.14159/12.*nphi;
+  
+         //float xprime = x*cos(delta_phi)-y*sin(delta_phi);
+         float yprime = x*sin(delta_phi)+y*cos(delta_phi);
+    
+         float m_energyc[5] = {14.1607,17.6719,23.0927,24.9074,28.0001};
+         float m_energyf[5] = {20.3787,18.6048,19.045,20.4087,23.8197};
+         float s_energyc[5] = {18.0425,24.4413,36.4643,33.8902,36.6783};
+         float s_energyf[5] = {20.9336,20.1998,22.8853,26.9295,35.3377};
+     //
+         float tenergyc[5];
+         float tenergyf[5];
+    
+         for (int k=0; k<5; k++){
+           tenergyc[k]=m_energyc[k]+2*s_energyc[k];
+           tenergyf[k]=m_energyf[k]+2*s_energyf[k];
+           }	    
+                                                                          
+         if (z>-500 && z<500){ //Central region
+         if (yprime<1550) if (calhit->getEnergy()>tenergyc[0]*0.001) pass = true; 
+         if (yprime>1550 && yprime<1600 ) if (calhit->getEnergy()>tenergyc[1]*0.001) pass = true;
+         if (yprime>1600 && yprime<1630) if (calhit->getEnergy()>tenergyc[2]*0.001) pass = true;
+         if (yprime>1630 && yprime<1660) if (calhit->getEnergy()>tenergyc[3]*0.001) pass = true;
+         if (yprime>1660) if (calhit->getEnergy()>tenergyc[4]*0.001) pass = true;
+                                                                                  }
+  
+         if (z<-500 || z>500){ //Forward region
+         if (yprime<1550) if (calhit->getEnergy()>tenergyf[0]*0.001) pass = true;
+         if (yprime>1550 && yprime<1600 ) if (calhit->getEnergy()>tenergyf[1]*0.001) pass = true;
+         if (yprime>1600 && yprime<1630) if (calhit->getEnergy()>tenergyf[2]*0.001) pass = true;
+         if (yprime>1630 && yprime<1660) if (calhit->getEnergy()>tenergyf[3]*0.001) pass = true;
+         if (yprime>1660) if (calhit->getEnergy()>tenergyf[4]*0.001) pass = true;
+          }	  
+         
+        }
+     
+        return pass;
+     
+ }
 
 
 void khurshid_works::init() {
@@ -110,13 +243,13 @@ void khurshid_works::processEvent( LCEvent * evt ) {
        		 
                 	 if ( hit_copy->getEnergy() > 0.5 ) {
             			ecalcol_kh->addElement(hit_copy); 
- 	   		 	std::cout << "Found " << hit_copy<< " ECalorimeter hits above 0.5 GeV." << std::endl;
+ 	   		 	//std::cout << "Found " << hit_copy<< " ECalorimeter hits above 0.5 GeV." << std::endl;
          			}
     
    			}
 
   		std::string initString = inputCol->getParameters().getStringVal(LCIO::CellIDEncoding);
-  //		std::cout << "crush point 3 " << std::endl;
+  
 
   		ecalcol_kh->parameters().setValue(LCIO::CellIDEncoding,initString);
   		evt->addCollection(ecalcol_kh,_outputEcalCollections[i].c_str());
@@ -125,7 +258,7 @@ void khurshid_works::processEvent( LCEvent * evt ) {
    		}
 
   	 catch (DataNotAvailableException &e){
-      		streamlog_out(DEBUG) << "could not find input ECAL collection " << colName << std::endl;
+      		streamlog_out(DEBUG) << "could not find the input ECAL collection " << colName << std::endl;
       		}   
 
    }	
